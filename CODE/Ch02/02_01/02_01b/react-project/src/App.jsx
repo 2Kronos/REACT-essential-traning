e121a9e378d11b8be33f476dd60c6a0ec55f1e5a
2Kronos <rayachrinovic@gmail.com>
2025-08-18 23:35:36 +0200

    modify

--- CODE/Ch02/02_01/02_01b/react-project/src/App.jsx
+++ CODE/Ch02/02_01/02_01b/react-project/src/App.jsx
@@ -1,12 +1,36 @@
 
 import './App.css'
 
-function App() {
+function Header({name, year}){
   
+  return(
+    <header>
+      <h1>{name} Kitchen</h1>
+      <p>Copyright {year} </p>
+    </header>
+  )
+}
 
-  return (
-   <h1>Hello react</h1>
-  );
+const items = [" Macoroni and cheese",
+  "Pizza",
+  "Sushi"
+];
+
+function Main({dishes}){
+  return(
+    <ul>
+     {dishes.map((dish)=> (<li style={{listStyle: "none"}}>{dish}</li>))}
+    </ul>
+  )
+}
+
+function App() {
+
+  return ( 
+  <div>
+    <Header name = 'Sunset' year = {new Date().getFullYear()}/>
+    <Main dishes = {items} /> 
+  </div>);
 }
 
 export default App
